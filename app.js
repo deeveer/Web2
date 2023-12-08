4284678a054d6a3cdb3d4f4716f16be2d2e1883f
Your Name <d2c2b2@example.com>
2023-12-09 01:55:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send(`hello`)
 })
+app.get('/test',(req,res)=>{
+    res.send(`test`)
+})
 app.listen(8000,()=>{
     console.log(`start at port 8000`)
 })
